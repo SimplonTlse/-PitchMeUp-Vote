c9d56098b4646f6439a0450069810d0272006e18
Ali Samar <samarsamar.aly@gmail.com>
2016-02-19 17:20:04 +0100

    Ajout des projets et modifications des boutons

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
 "use strict"
 $(document).ready(function(){
 
-	$('.cache').hide();
+	var $cache = $('.cache');
+	$cache.hide();
 
 	$('.btn').on('click', function(event){
 		event.preventDefault();
 		resetCacheColor();
-		$('.cache').show();
+		$cache.show();
 		var id = ($(this).attr('id'));
 		if (id == 'ok') {
-			$('.cache').addClass('bg-green');
+			$cache.addClass('bg-green');
 		} else if (id == "pascompris") {
-			$('.cache').addClass('bg-orange');
+			$cache.addClass('bg-orange');
 		} else if (id == "non") {
-			$('.cache').addClass('bg-red');
+			$cache.addClass('bg-red');
 		}
 
 	});
 
 	function resetCacheColor(){
-		$('.cache').removeClass('bg-red');
-		$('.cache').removeClass('bg-green');
-		$('.cache').removeClass('bg-orange');
+		$cache.removeClass('bg-red');
+		$cache.removeClass('bg-green');
+		$cache.removeClass('bg-orange');
 	}
 
-	$('.cache').click(function(){
-		$('.cache').hide();
+	$cache.click(function(){
+		$cache.hide();
 	});
 
 });
