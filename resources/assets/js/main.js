6832a78a5df4ccf6f51c9dc8e4eb1e36047a66a9
Ali Samar <samarsamar.aly@gmail.com>
2016-05-24 17:23:29 +0200

    Interactions choix vote/vote final : react

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,63 +1,11 @@
-'use strict'
-var React = require('react');
-var ReactDOM = require('react-dom');
+let Wrapper = require('./vote.js');
+let React = require('react');
+let ReactDOM = require('react-dom');
+let node = document.querySelector("[x-react-vote]");
 
-//fonction pour redirect sur page vote
-var Vote = React.createClass({
-	 getInitialState: function(){
-	 	return {query: ''}
-	 },
-	 queryChange: function(evenement){
-	 	this.setState({query: evenement.target.value});
-	 },
-	 lien: function(){
-	 	return this.state.query+'/vote.html';
-	 },
-    render: function() {
-        return (
-            <div className="bouton">
-                <a href={this.lien()} id="vote" className="waves-effect waves-light btn grey">
-                <i className="material-icons right">offline_pin</i>Votez</a>
-            </div>
-        );
-    }
-});
-
-//fonction pour redirect sur page compte a rebourss
-var CompteARebours = React.createClass({
-	getInitialState: function(){
-	 	return {query: ''}
-	 },
-	 queryChange: function(evenement){
-	 	this.setState({query: evenement.target.value});
-	 },
-
-	 lien: function(){
-	 	return this.state.query+'/car.html';
-	 },
-    render: function() {
-        return (
-            <div className="bouton">
-                <a href={this.lien()} id="car" className="waves-effect waves-light btn grey">
-                <i className="material-icons right">alarm_on</i>Compte à rebours</a>
-            </div>
-        );
-    }
-});
-
-//affichage complet
-var Wrapper = React.createClass({
-	render: function() {
-        return (
-            <div>
-                <Vote />
-                <CompteARebours />
-            </div>
-        );
-    }
-});
-
-ReactDOM.render(
-    <Wrapper />, 
-    document.getElementById('btns')
-);
+if (node) {
+    ReactDOM.render(
+        <Wrapper />, 
+        node
+    );
+};
