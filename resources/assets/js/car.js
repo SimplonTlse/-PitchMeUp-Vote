19c6691891244d08cf55fa3fababbe11daa0f15b
Ali Samar <samarsamar.aly@gmail.com>
2016-06-09 11:55:49 +0200

    Utilisation moment js(en cours)

--- resources/assets/js/car.js
+++ resources/assets/js/car.js
@@ -1,46 +1,52 @@
 'use strict'
 let React = require('react');
 let ReactDOM = require('react-dom');
+let moment = require('moment');
 let $ = require('jquery');
 
 let CountdownTimer = React.createClass({
 	getInitialState: function() {
+		console.log(moment());
 		return {
-			minutes: 1,
-			seconds: 10,
-			dixiemeSeconde: 0
+			// minutes: 0,
+			// seconds: 10,
+			dixiemeSeconde: 0,
+			startMoment: moment(),
+			startDuration: moment.duration(5000*60)
 		};
 	},
-	tick: function() {
+	tic: function() {
+
+		let now = moment();
+		let diff = now.diff(this.state.startMoment);
+		//console.log(diff);
+		
+		let duration = moment.duration(this.state.startDuration).subtract(diff);
+		
+		//console.log(this.state.startDuration);
+
+		//console.log(duration.minutes(),duration.seconds());
+		let min = duration.minutes();
+		console.log(min);
+	},
+	tac: function() {
 		this.setState({dixiemeSeconde: this.state.dixiemeSeconde - 1});
-
-		if (this.state.dixiemeSeconde < 0) {
-			this.state.dixiemeSeconde = 9;
-			//this.state.seconds = this.state.seconds - 1;
+		if (this.state.dixiemeSeconde < 1) {
+			this.state.dixiemeSeconde = 10;
 		}		
-			
-		this.setState({seconds: this.state.seconds - 1});
-		if (this.state.seconds <= 0) {
-			this.state.seconds = 59;
-			this.state.minutes = this.state.minutes - 1;
-		}
 
-		if (this.state.minutes === 0 && this.state.seconds === 0 && this.state.dixiemeSeconde === 0) {
-			clearInterval(this.interval);
-		}
 	},
 	componentDidMount: function() {
-		this.setState({minutes: this.state.minutes});
-		this.setState({seconds: this.state.seconds});
 		this.setState({dixiemeSeconde: this.state.dixiemeSeconde});
-		let interval = setInterval(this.tick, 1000);
+		this.interval = setInterval(this.tic, 1000);
+		this.intervall = setInterval(this.tac, 100);
 	},
 	componentWillMount: function() {
 		clearInterval(this.interval);
 	},
 	render: function() {
 		return (
-			<div>{this.state.minutes} : {this.state.seconds} : {this.state.dixiemeSeconde}</div>
+			<div>{this.state.min} : {this.state.duration} . {this.state.dixiemeSeconde}</div>
 			// <div className="boutons">
    //          	<Bouton className="play" color="green" icon="play_arrow" text="Démarrer" trigger={this.start} />
    //          	<Bouton className="init" color="black" icon="replay" text="Réinitialiser" trigger={this.componentWillMount} />
